ad8103182517837b363e5f55ca50c5c231d8275b
fuuudgeicecream <josh211505857@gmail.com>
2016-12-30 17:10:32 +0200

    k

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
 
+  var config = {
+    apiKey: "AIzaSyDUq7iXyyK_oXSVyeftb5hj5rplz8o3a3M",
+    authDomain: "contacts-ad20d.firebaseapp.com",
+    databaseURL: "https://contacts-ad20d.firebaseio.com",
+    storageBucket: "contacts-ad20d.appspot.com",
+    messagingSenderId: "414241529872"
+  };
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +19,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
