072cdbcb9c406bef408491bfbce5665ba7fc136e
fuuudgeicecream <josh211505857@gmail.com>
2017-01-03 09:53:59 +0200

    Filtering Businesses by Categories.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from './services/firebase.service';
+import {Category} from './Category';
+import {Business} from './Business';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers:[FirebaseService],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent implements OnInit{
+  businesses: Business[];
+  categories: Category[];
+  appState: string;
+  activeKey: string;
+    constructor(private _firebaseService: FirebaseService) {
+    
+  }
+  ngOnInit(){
+    this._firebaseService.getBusinesses().subscribe(businesses=>{
+       this.businesses = businesses;
+    });
+    this._firebaseService.getCategories().subscribe(categories=>{
+     	this.categories = categories;
+    });
+  }
+  changeState(state, key){
+    console.log("Changing State to: " + state );
+    if(key){
+          console.log("Changing key to: " + key );
+      this.activeKey = key;
+    }
+    this.appState = state;
+    }
+    filterCategory(category){
+    this._firebaseService.getBusinesses(category).subscribe(businesses=>{
+       this.businesses = businesses;
+       });
+    }
+  }
